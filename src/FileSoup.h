4ef78291c0c9cb3c94fa46e64616c09627477e47
Karidus-423 <kapuerto23@gmail.com>
2025-09-12 17:42:27 -0400

    sync

--- src/FileSoup.h
+++ src/FileSoup.h
@@ -1,23 +1,35 @@
-
 #ifndef FILESOUP
 #define FILESOUP
 
 #include <cstdio>
+
+enum FILESOUP_ERRORS{
+	FILE_NOT_FOUND,
+	READ_ACCESS_DENIED,
+	OPEN_ACCESS_DENIED,
+	WRITE_ACCESS_DENIED,
+};
+
 typedef struct {
 	void* location;
-}soupfile;
+}filesoup;
 
 //Standard file handling.
-soupfile* OpenFileSoup(char* filename);
+filesoup* OpenFileSoup(char* filename);
 
-soupfile* ReadFileSoup(char* filename);
+filesoup* ReadFileSoup(char* filename);
 
-//Reads the contents of a file and passed a character buffer of the contents.
+//Reads the contents of a file and passes a character array of the contents.
 //Closes and opens the file when the function returns.
 char* CopyFileSoup(char* filename);
 
 #endif
 
-#ifdef FILESOUP_IMPLEMENTATION
+#ifdef FILESOUP_IMPLEMENTED
+char* CopyFileSoup(char* filename){
+	//Open file
+	//Copy file contents
+	//Return file contents.
+}
 
 #endif
---
+++ src/TestSoup.h
@@ -0,0 +1,18 @@
+#ifndef TESTSOUP
+#define TESTSOUP
+
+#endif
+
+#ifdef TESTSOUP_IMPLEMENTED
+
+//TODO
+//Black Box Testing Methodologies
+//TODO
+//White Box Testing Methodologies
+
+//TODO: Find a way to make sure they are the same type passed.
+//probably wrap it with a macro.
+bool ExpectTest(void* real,void* expected){
+}
+
+#endif // TESTSOUP
